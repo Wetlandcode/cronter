1947b866cc203cab86afd6e2c175747a6cdd6d5d
Wetlandcode <100895237+Wetlandcode@users.noreply.github.com>
2022-04-07 14:46:23 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const { Message } = require("discord.js");
+const fs = require("fs");
+
+module.exports = async (client, message) => {
+    
+    client.user.setActivity(`??help | Dyno Premium 2.0`, { type : "WATCHING" });
+   
+     /* client.ws.on('INTERACTION_CREATE', async (interaction) => {
+   client.slash.commandsrun(interaction, client);
+  
+    })   */       
+             
+             
+ 
+
+   
+}
