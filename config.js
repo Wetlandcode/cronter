fa2c52b7842492945d8666eb1b447e32a5e8d115
Wetlandcode <100895237+Wetlandcode@users.noreply.github.com>
2022-04-08 18:44:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "registercommands" : false,
-     "token": "OTU5NDc5NDk5MDQ3NzIzMDI4.Ykce6g.kzfpKKuIMRM5gRt9IV1XpLPwNUU",
+     "token": "OTU5NDc5NDk5MDQ3NzIzMDI4.Ykce6g.I4XdUkeXRaKT7wn9sla5s_Jop3A",
     "imageapi": "bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d",
     "ownerID": ["940891247722258493"],
 "prefix": "?",
